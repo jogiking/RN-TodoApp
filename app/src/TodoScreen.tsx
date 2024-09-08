f7791c031085a76fe6deaaedcf9885fbbf718b63
jogiking <whrldhks38@gmail.com>
2024-09-08 19:40:36 +0900

    feat: 추가버튼 눌러 새로운 항목 추가되게 하기

--- app/src/TodoScreen.tsx
+++ app/src/TodoScreen.tsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import ListView from "./ListView";
 import { TodoItem } from "./ListView";
 
 interface TodoScreenProps {}
 
+const defaultList: TodoItem[] = [
+  {
+    content: "공부하기",
+    registerDate: "2024-09-08",
+  },
+  {
+    content: "운동하기",
+    registerDate: "2024-09-08",
+  },
+];
+
 function TodoScreen({}: TodoScreenProps) {
-  const todoList: TodoItem[] = [
-    {
-      content: "공부하기",
-      registerDate: "2024-09-08",
-    },
-    {
-      content: "운동하기",
-      registerDate: "2024-09-08",
-    },
-  ];
+  const [todoList, setTodoList] = useState<TodoItem[]>(defaultList);
+
+  const addTodo = () => {
+    const newTodo: TodoItem = {
+      content: "새로운 할 일",
+      registerDate: "2024-09-09",
+    };
+    setTodoList([newTodo, ...todoList]);
+  };
 
   return (
     <View style={styles.container}>
       <ListView style={styles.listViewContainer} items={todoList} />
-      <TouchableOpacity style={styles.addContainer}>
+      <TouchableOpacity style={styles.addContainer} onPress={addTodo}>
         <Text style={styles.addText}>+</Text>
       </TouchableOpacity>
     </View>
